eec1b0e3673ff7a4cd06e56d9ebaa535f174260c
AidenLai2305 <aiden.lai2305@gmail.com>
2025-05-30 18:15:11 -0700

    renaming files

--- results.js
+++ results.js
@@ -5,7 +5,7 @@ const margin = { top: 20, right: 30, bottom: 50, left: 70 },
       width = 800 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-const svg = d3.select("#densitytype2")
+const svg = d3.select("#resultsgraph")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
