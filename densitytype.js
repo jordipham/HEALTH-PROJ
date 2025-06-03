24c9caa8b9d7cfbc5079a912f561ae8037de077a
AidenLai2305 <aiden.lai2305@gmail.com>
2025-06-02 23:40:22 -0700

    trying css width scale

--- densitytype.js
+++ densitytype.js
@@ -1,9 +1,17 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 // Set dimensions
-const margin = { top: 20, right: 30, bottom: 50, left: 60 },
-      width = window.innerWidth * 0.45 - margin.left - margin.right, 
-      height = window.innerWidth * 0.27 - margin.top - margin.bottom;
+// const margin = { top: 20, right: 30, bottom: 50, left: 60 },
+//       width = window.innerWidth * 0.45 - margin.left - margin.right, 
+//       height = window.innerWidth * 0.27 - margin.top - margin.bottom;
+
+
+const margin = { top: 20, right: 30, bottom: 50, left: 60 };
+
+const container = document.getElementById("grid-item-2").parentElement; // or 'densitytype'
+const boundingBox = container.getBoundingClientRect();
+const width = (boundingBox.width * .45) - margin.left - margin.right;
+const height = (boundingBox.width * .27) - margin.top - margin.bottom; // Adjust ratio as needed
 
 const svg = d3
   .select("#densitytype")
